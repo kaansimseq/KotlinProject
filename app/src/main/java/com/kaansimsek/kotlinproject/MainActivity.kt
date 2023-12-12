f2e05536ad5ebbdca2ce5d8639384c615975efa8
cagangurbuz <mehmet.cagan.gurbuz@gmail.com>
2023-12-12 17:08:30 +0300

    Bottom navigation bar

--- app/src/main/java/com/kaansimsek/kotlinproject/MainActivity.kt
+++ app/src/main/java/com/kaansimsek/kotlinproject/MainActivity.kt
@@ -2,10 +2,44 @@ package com.kaansimsek.kotlinproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.kaansimsek.kotlinproject.databinding.ActivityAppDesc1Binding
+import com.kaansimsek.kotlinproject.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavView.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.map -> replaceFragment(MapFragment())
+                R.id.profile -> replaceFragment(ProfileFragment())
+
+                else ->{
+
+                }
+
+            }
+            true
+        }
+
     }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout,fragment)
+        fragmentTransaction.commit()
+    }
+
 }
