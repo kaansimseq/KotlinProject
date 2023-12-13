a4718dbb6b706c89ac10c926e985ac3daafc8c83
BurakTanis <burak.tanis@std.yeditepe.edu.tr>
2023-12-13 16:41:08 +0300

    created profile page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation("com.google.firebase:firebase-auth:22.2.0")
     implementation("com.google.firebase:firebase-firestore:24.9.1")
     implementation("com.google.firebase:firebase-storage:20.3.0")
+    implementation("com.google.firebase:firebase-database-ktx:20.3.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/kaansimsek/kotlinproject/ProfileFragment.kt
+++ app/src/main/java/com/kaansimsek/kotlinproject/ProfileFragment.kt
@@ -1,59 +1,130 @@
 package com.kaansimsek.kotlinproject
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.BitmapShader
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Shader
+import android.os.AsyncTask
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.firestore.*
+import com.kaansimsek.kotlinproject.databinding.ActivitySignupBinding
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.URL
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+// ... Diğer import ifadeleri ...
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private lateinit var nameTextView: TextView
+    private lateinit var emailTextView: TextView
+    private lateinit var biographyTextView: TextView
+    private lateinit var phonenumberTextView: TextView
+    private lateinit var profileImageView: ImageView
+
+    private var db = Firebase.firestore
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        // TextView'ları layout dosyasındaki elemanlarla eşleştir
+        nameTextView = view.findViewById(R.id.nameTextView)
+        emailTextView = view.findViewById(R.id.emailTextView)
+        biographyTextView = view.findViewById(R.id.biographyTextView)
+        phonenumberTextView = view.findViewById(R.id.phonenumberTextView)
+        profileImageView = view.findViewById(R.id.profileImageView)
+
+        val userId = FirebaseAuth.getInstance().currentUser?.uid
+        if (userId != null) {
+            val ref = db.collection("users").document(userId)
+            ref.get().addOnSuccessListener { documentSnapshot ->
+                if (documentSnapshot.exists()) {
+                    val name = documentSnapshot.getString("fullName")
+                    val email = documentSnapshot.getString("email")
+                    val biography = documentSnapshot.getString("biography")
+                    val phonenumber = documentSnapshot.getString("phoneNumber")
+                    val image = documentSnapshot.getString("imageUrl")
+
+                    // Null olmayan değerleri TextView'lara atama
+                    nameTextView.text = "${name ?: ""}"
+                    emailTextView.text = "MAIL: ${email ?: ""}"
+                    biographyTextView.text = " ${biography ?: ""}"
+                    phonenumberTextView.text = "CALL: ${phonenumber ?: ""}"
+
+                    // Resmi yükleme işlemi
+                    if (!image.isNullOrEmpty()) {
+                        LoadImageTask().execute(image)
+                    }
+                }
+            }.addOnFailureListener { exception ->
+                // Hata durumunda Toast mesajı göster
+                Toast.makeText(requireContext(), "Failed: ${exception.message}", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        return view
+    }
+
+    private inner class LoadImageTask : AsyncTask<String, Void, Bitmap>() {
+        override fun doInBackground(vararg params: String?): Bitmap? {
+            val imageUrl = params[0]
+            return try {
+                val connection = URL(imageUrl).openConnection() as HttpURLConnection
+                connection.connect()
+                val inputStream = connection.inputStream
+                BitmapFactory.decodeStream(inputStream)
+            } catch (e: IOException) {
+                e.printStackTrace()
+                null
+            }
+        }
+
+        override fun onPostExecute(result: Bitmap?) {
+            result?.let {
+                // Bitmap'i yuvarlak yap
+                val roundedBitmap = getRoundedBitmap(it)
+
+                // Yuvarlak Bitmap'i ImageView içinde gösterme
+                profileImageView.setImageBitmap(roundedBitmap)
+            }
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
-    }
+    fun getRoundedBitmap(bitmap: Bitmap): Bitmap {
+        val roundedBitmap = Bitmap.createBitmap(bitmap.width, bitmap.height, bitmap.config)
+        val canvas = Canvas(roundedBitmap)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val paint = Paint()
+        paint.isAntiAlias = true
+        paint.shader = BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
+
+        val rect = RectF(0f, 0f, bitmap.width.toFloat(), bitmap.height.toFloat())
+        canvas.drawRoundRect(rect, bitmap.width.toFloat() / 2, bitmap.height.toFloat() / 2, paint)
+
+        return roundedBitmap
     }
 }
+
+
